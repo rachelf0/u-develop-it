cbc95024cf2970930d784bf8e0b564793c5bce13
Rachel Fritz <rachelfritz0@gmail.com>
2020-08-19 09:35:19 -0500

    final routes for voteRoutes.js added

--- routes/apiRoutes/index.js
+++ routes/apiRoutes/index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
 router.use(require('./voterRoutes'));
-
 router.use(require('./candidateRoutes'));
+router.use(require('./voteRoutes'));
 
 module.exports = router;
